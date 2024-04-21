6cfa7201b0998ddd2045de64be23c83b92df68e4
Sidharth pandro <sidharthpandro50@gmail.com>
2024-04-21 09:57:11 +0530

    landing page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,17 @@
-import Navbar from "../components/Navbar"
+import Navbar from "../components/Navbar";
 import Background from "../assets/Background.png";
+import LandingPage from "../components/LandingPage";
 
 export default function Home() {
-
-    return (
-        <>
-        <div className="bg-[#FEBC9A] h-lvh	">
-            <div className="absolute left-[35%]	">
-         <img src={Background} alt="bg-icon image" />
-         </div>
-            <Navbar />
+  return (
+    <>
+      <div className="bg-[#FEBC9A] h-lvh	">
+        <div className="absolute left-[35%]	">
+          <img src={Background} alt="bg-icon image" />
         </div>
-        </>
-    )
+        <Navbar />
+        <LandingPage />
+      </div>
+    </>
+  );
 }
